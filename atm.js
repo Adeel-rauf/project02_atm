96fae9d260832ce219ca6b9a35dfa6b9c498d832
Adeel-rauf <153929392+Adeel-rauf@users.noreply.github.com>
2024-01-06 10:41:12 +0500

    Add files via upload
    Project_02: Atm (Done with Inquirer and if else if chain)

---
+++ atm.js
@@ -0,0 +1,81 @@
+import inquirer from "inquirer";
+let atm = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'username',
+        message: 'please enter your name',
+    },
+    {
+        type: 'input',
+        name: 'pin',
+        message: `please enter your pin`,
+    },
+    {
+        type: 'list',
+        name: 'acctype',
+        choices: ['current', 'saving'],
+        message: 'plz choose your account type',
+    },
+    {
+        type: 'list',
+        name: 'paymethod',
+        choices: ['Fast cash', 'Selective cash'],
+        message: 'Select your mode of payment',
+        when(atm) {
+            return atm.acctype;
+        }
+    },
+    {
+        type: 'list',
+        name: 'amount',
+        choices: [500, 1000, 1500, 2000, 5000, 10000, 25000],
+        message: 'Select amount you want to withdraw',
+        when(atm) {
+            return atm.paymethod == 'Fast cash';
+        }
+    },
+    {
+        type: 'number',
+        name: 'amount',
+        message: 'Enter amount you want to withdraw',
+        when(atm) {
+            return atm.paymethod == 'Selective cash';
+        }
+    },
+]);
+if (atm.username == 'adeel' && atm.pin == 123) {
+    const balance = 100000;
+    const selectedamount = atm.amount;
+    const remaining = balance - selectedamount;
+    if (selectedamount < balance) {
+        console.log(`you have withdrawn ${selectedamount}`);
+        console.log(`your remaining balance is ${remaining}`);
+    }
+    else if (selectedamount > balance) {
+        console.log('Insufficient balance');
+    }
+}
+else if (atm.username == 'chaman' && atm.pin == 321) {
+    const balance = 9000;
+    const selectedamount = atm.amount;
+    const remaining = balance - selectedamount;
+    if (selectedamount < balance) {
+        console.log(`you have withdrawn ${selectedamount}`);
+        console.log(`your remaining balance is ${remaining}`);
+    }
+    else if (selectedamount > balance) {
+        console.log('Insufficient balance');
+    }
+}
+else if (atm.username && atm.pin) {
+    const balance = 20000;
+    const selectedamount = atm.amount;
+    const remaining = balance - selectedamount;
+    if (selectedamount < balance) {
+        console.log(`you have withdrawn ${selectedamount}`);
+        console.log(`your remaining balance is ${remaining}`);
+    }
+    else if (selectedamount > balance) {
+        console.log('Insufficient balance');
+    }
+}
